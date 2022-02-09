c12e063ef44e96c511cc123bdb60c17aacb21a62
Michael Paukov <michael.paukov@gmail.com>
2022-02-09 15:07:23 +0100

    some changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,23 @@
 import { Component } from 'react';
+import { nanoid } from 'nanoid';
 
 class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
     name: '',
+    number: '',
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+    console.log(e.target);
   };
 
   render() {
@@ -16,13 +30,19 @@ class App extends Component {
           alignItems: 'center',
         }}
       >
-        <input
-          type="text"
-          name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-        />
+        <form action="submit" onSubmit={this.handleSubmit}>
+          <label>
+            Имя
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          </label>
+          <button type="submit">Сохранить</button>
+        </form>
       </div>
     );
   }
